5f3896c46dae04cda8b9db159ae3a4e01618e811
judemlim <judemlim@gmail.com>
2019-05-31 10:46:26 +1000

    random quote generator update

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -4,6 +4,7 @@ import portfolioImg from './images/portfolio.png';
 import cvImg from './images/cv.png';
 import aboutImg from './images/about.png';
 import './Home.css';
+import quotes from './quotes.json';
 
 interface Props {
   homeHeader(): void;
@@ -16,12 +17,9 @@ const Home: React.FC<Props> = (props: Props) => {
 
   useEffect(() => {
     props.homeHeader();
-    fetch('https://quotes.rest/qod.json')
-      .then(response => response.json())
-      .then(myJson => {
-        setQuote(myJson.contents.quotes[0].quote);
-        setAuthor(myJson.contents.quotes[0].author);
-      });
+    let item = quotes.quotes[Math.floor(Math.random() * quotes.quotes.length)];
+    setQuote(item.quote);
+    setAuthor(item.author);
     return () => {
       props.awayHeader();
     };
@@ -30,15 +28,10 @@ const Home: React.FC<Props> = (props: Props) => {
   const content = (
     <main className="home-page">
       <br />
-      <div className="qod-wrap">
-        <p className="qod-quote">”{quote}”</p>
-        <p className="qod-author">~ {author}</p>
-        <p className="qod-acknowledgement">
-          Quotes of the day from{' '}
-          <a href="https://theysaidso.com/api/" target="_blank" rel="noopener noreferrer">
-            theysaidso.com/api/
-          </a>
-        </p>
+      <div className="rqg-wrap">
+        <p className="rqg-quote">“{quote}”</p>
+        <p className="rqg-author">~ {author}</p>
+        <p className="rqg-info">Random Quote Generator</p>
       </div>
       <div className="home-wrap">
         <HomeCard title="Portfolio" path="/portfolio" imgPath={portfolioImg} />
