f079fd4bfa0c2b3e0733a0d754c821d6ffaff414
judemlim <judemlim@gmail.com>
2020-01-09 19:12:14 +1100

    fixes

--- src/components/WebsiteInformation.tsx
+++ src/components/WebsiteInformation.tsx
@@ -5,11 +5,11 @@ const WebsiteInformation = () => {
     <main className="website-information">
       <div className="content-wrap">
         <h1>Website Information</h1>
-        <h2>General Information aoeu</h2>
+        <h2>General Information</h2>
         <p>
           This website was created using Facebook’s <strong>React</strong> library. It is written primarily in{' '}
           <strong>TypeScript</strong> and mainly employs React stateful functional components as opposed to the more
-          wide-spread class based components aoeu aoeu . aeou a,.u .a,.
+          wide-spread class based components.
         </p>
         <p>
           All the programming language features I display on this website, <strong>HTML</strong>, <strong>CSS</strong>,{' '}
