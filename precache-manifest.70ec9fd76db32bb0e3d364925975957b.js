2ba5323540b77a85063618ae9446518ed8e3bf14
judemlim <judemlim@gmail.com>
2020-01-10 19:38:21 +1100

    Updates

--- precache-manifest.70ec9fd76db32bb0e3d364925975957b.js
+++ precache-manifest.70ec9fd76db32bb0e3d364925975957b.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9df742e4a3a76ac46130a6b7dfb7172f",
+    "revision": "5f3b68a63e0664abff784bf58c232549",
     "url": "/index.html"
   },
   {
-    "revision": "b2e1196161e71c25eb92",
-    "url": "/static/css/main.dd7cfd11.chunk.css"
+    "revision": "f28e3d8e3c1a7a2fc8ab",
+    "url": "/static/css/main.41576241.chunk.css"
   },
   {
-    "revision": "e63d086d852f7bc4be09",
-    "url": "/static/js/2.c21c9e8f.chunk.js"
+    "revision": "73583ebc74eb43a9fa4c",
+    "url": "/static/js/2.795b1418.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/static/js/2.c21c9e8f.chunk.js.LICENSE"
+    "url": "/static/js/2.795b1418.chunk.js.LICENSE"
   },
   {
-    "revision": "b2e1196161e71c25eb92",
-    "url": "/static/js/main.829506f6.chunk.js"
+    "revision": "f28e3d8e3c1a7a2fc8ab",
+    "url": "/static/js/main.b39332c4.chunk.js"
   },
   {
     "revision": "b8d81126cfea448e0658",
@@ -26,6 +26,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "765728fe40d4e82e99648f171aabb7d5",
     "url": "/static/media/about.765728fe.png"
+  },
+  {
+    "revision": "ec84a0c17b490443fff10ddcacdeab12",
+    "url": "/static/media/chat-room.ec84a0c1.png"
   },
   {
     "revision": "6cb69f838a4957807ed9ae21fffafce3",
@@ -48,12 +52,32 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/masks.651ace53.png"
   },
   {
+    "revision": "8f2c83b06a1f685ef6af57af869b0d2b",
+    "url": "/static/media/opinion-mining.8f2c83b0.png"
+  },
+  {
+    "revision": "db8942f9f116afd3f95fd3ae023e5e1b",
+    "url": "/static/media/pattern-finder.db8942f9.png"
+  },
+  {
     "revision": "c96f9182151191bde476fbba29fa0f76",
     "url": "/static/media/portfolio.c96f9182.png"
   },
   {
+    "revision": "1a6115d731442ff83f3a23fb7659ab9e",
+    "url": "/static/media/roman-numerals.1a6115d7.png"
+  },
+  {
+    "revision": "ff8dbdee5b87b2317a8155a1a19b9af4",
+    "url": "/static/media/scheduling.ff8dbdee.png"
+  },
+  {
     "revision": "1e55bb8a39412e5969c8f24bcd1af5b3",
     "url": "/static/media/todo-list.1e55bb8a.png"
+  },
+  {
+    "revision": "9b48b7cb3b5e7d88cd71071bd18636a4",
+    "url": "/static/media/trip-planner.9b48b7cb.png"
   },
   {
     "revision": "4ab78ae90b1e99faab38b0cdcb32c94d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.61aaf65f23791ab63966916a96cd12f7.js"
+  "/precache-manifest.70ec9fd76db32bb0e3d364925975957b.js"
 );
 
 self.addEventListener('message', (event) => {
