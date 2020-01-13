5131770ef94b029060ef25dcaf973e34e7ba8043
judemlim <judemlim@gmail.com>
2020-01-13 17:18:08 +1100

    cv link update

--- src/components/CV.tsx
+++ src/components/CV.tsx
@@ -3,11 +3,7 @@ import React from 'react';
 const CV: React.FC = () => (
   <main className="cv center">
     <h1>CV</h1>
-    <iframe
-      src="https://drive.google.com/file/d/1akj2HXrMtXQc1dUDm8XZ7Y0z3WvmloUE/view?usp=sharing"
-      width="800"
-      height="600"
-    />
+    <iframe src="https://drive.google.com/file/d/1akj2HXrMtXQc1dUDm8XZ7Y0z3WvmloUE/preview" width="800" height="600" />
     <br />
   </main>
 );
